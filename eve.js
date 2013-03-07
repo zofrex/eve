7d82c0a45e9772227c78df2afc5d574b70443a23
zofrex <zofrex@gmail.com>
2013-03-07 22:47:52 +0000

    Make logging colorful and neat

--- eve.js
+++ eve.js
@@ -2,6 +2,8 @@
 
 var url = require('url');
 var http = require('http');
+var colors = require('colors');
+
 var acceptor = http.createServer().listen(8888);
 
 acceptor.on('request', function(request, response) {
@@ -16,16 +18,39 @@ acceptor.on('request', function(request, response) {
 		response.writeHeader(serverResponse.statusCode, serverResponse.headers);
 		serverResponse.pipe(response);
 		serverResponse.resume();
-		console.log(request.method + ' ' + request.url);
-		console.log('Request:');
-		inspect(request.headers);
-		console.log('Response:');
-		inspect(serverResponse.headers);
+		log(options, request, serverResponse);
 	});
 	request.pipe(connector);
 	request.resume();
 });
 
-function inspect(thing) {
-	console.log(JSON.stringify(thing, null, 2));
+function log(options, request, response) {
+	var method = colorMethod(request.method);
+	console.log('');
+	console.log(method + ' ' + options.path);
+	console.log('');
+	console.log('Request headers:'.grey);
+	logHeaders(request.headers);
+	console.log('');
+	console.log('Response headers:'.grey);
+	logHeaders(response.headers);
+}
+
+function colorMethod(method) {
+	switch(method) {
+		case "GET":
+			return method.green;
+		case "POST":
+			return method.yellow;
+		case "PUT":
+			return method.cyan;
+		case "DELETE":
+			return method.red;
+	}
+}
+
+function logHeaders(headers) {
+	for(var name in headers) {
+		console.log(name + ': ' + headers[name]);
+	}
 }
