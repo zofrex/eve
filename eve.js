67a8438b2cd4cbb28614033155dd086af5570b7c
zofrex <zofrex@gmail.com>
2013-03-08 01:45:00 +0000

    Add command-line option to switch between proxy and target host mode

--- eve.js
+++ eve.js
@@ -3,13 +3,48 @@ var http = require('http');
 var colors = require('colors');
 var _ = require('underscore');
 
+var optimist = require('optimist');
+
+optimist
+	.usage('Supply either --proxy-mode or --target-host')
+	
+	.boolean('proxy-mode')
+	.describe('proxy-mode', 'Operate as an HTTP proxy')
+	
+	.default('port', 8888)
+	.describe('port', 'Port to listen on')
+	.alias('p', 'port')
+
+  .default('host', '0.0.0.0')
+	.describe('host', 'Address to listen on (0.0.0.0 if ommitted)')
+	.alias('h', 'host')
+
+	.string('target-host')
+	.describe('target-host', 'Host to forward requests to')
+	.alias('t', 'target-host')
+;
+
+var argv = optimist.argv;
+
+if(!argv['proxy-mode']) {
+	optimist.demand('target-host').argv;
+}
+
 var acceptor = http.createServer().listen(8888);
 var width = 0;
 
 acceptor.on('request', function(request, response) {
 	request.pause();
-	var options = url.parse(request.url);
+	var options;
+	if(!argv['proxy-mode']) {
+		var path = url.parse(request.url).path;
+		options = url.parse(argv['target-host'] + path);
+	}
+	else {
+		options = url.parse(request.url);
+	}
 	options.headers = request.headers;
+	options.headers.host = options.hostname;
 	options.method = request.method;
 	options.agent = false;
 
